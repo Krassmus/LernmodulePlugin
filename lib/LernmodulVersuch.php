36385362e2e0fba2a8bdf061a41adc927a1b72e3
Rasmus Fuhse <fuhse@data-quest.de>
2016-12-07 11:32:51 +0100

    disable cleanup

--- lib/LernmodulVersuch.php
+++ lib/LernmodulVersuch.php
@@ -4,7 +4,7 @@ class LernmodulVersuch extends SimpleORMap {
 
     static public function cleanUpDatabase()
     {
-        self::deleteBySQL("chdate <= mkdate + 20 AND mkdate < UNIX_TIMESTAMP() - 60 * 2");
+        //self::deleteBySQL("successful IS NULL AND chdate <= mkdate + 20 AND mkdate < UNIX_TIMESTAMP() - 60 * 2");
     }
 
     static protected function configure($config = array())
