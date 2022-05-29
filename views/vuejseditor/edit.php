1be0d917473f277a87e85645046838d21559b4dc
Ann Yanich <ann@yanich.de>
2022-05-29 16:07:08 +0200

    Add some navigation links to the vue.js editor page

--- views/vuejseditor/edit.php
+++ views/vuejseditor/edit.php
@@ -10,7 +10,7 @@
 
 
 <script>
-    STUDIP.LernmoduleVueJS = <?= json_encode($javascript_global_variables) ?>
+  STUDIP.LernmoduleVueJS = <?= json_encode($javascript_global_variables) ?>;
 </script>
 
 <? if (!$plugin->config[production]) : ?>
@@ -23,3 +23,28 @@
   <script src="<?= $plugin->getPluginUrl() . '/vue/dist/js/chunk-common.js' ?>"></script>
   <link rel="stylesheet" href="<?= $plugin->getPluginUrl() . '/vue/dist/css/editor.css' ?>"></link>
 <? endif ?>
+
+<?php
+$actions = new ActionsWidget();
+$actions->addLink(
+    dgettext("lernmoduleplugin", "Abspieloptionen bearbeiten"),
+    PluginEngine::getURL($plugin, array(), "lernmodule/edit/" . $mod->getId()),
+    Icon::create("edit", "clickable")
+);
+Sidebar::get()->addWidget($actions);
+
+$views = new ViewsWidget();
+$views->addLink(
+    $mod['name'],
+    PluginEngine::getURL($plugin, array(), "lernmodule/view/" . $mod->getId()),
+    null,
+    array()
+);
+$views->addLink(
+    dgettext("lernmoduleplugin", "Auswertung"),
+    PluginEngine::getURL($plugin, array(), "lernmodule/evaluation/" . $mod->getId()),
+    null,
+    array()
+);
+
+Sidebar::Get()->addWidget($views);
