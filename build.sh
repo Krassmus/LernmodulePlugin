3063b10e8b6374b1dba33612575f1e9b686cc764
Ann Yanich <ann@yanich.de>
2022-05-30 18:14:29 +0200

    Add comment to build.sh

--- build.sh
+++ build.sh
@@ -6,4 +6,5 @@ timestamp=$(date "+%Y.%m.%d-%H.%M.%S")
 # Stud.IP takes FOREVER to install the plugin if .git or node_modules are included.
 # zip -r "dist/plugin-$timestamp.zip" . -x "./.git/*" "./vue/node_modules/*" "./.idea/*" "./dist/*"
 zip -r "dist/plugin.zip" . -x "./.git/*" "./vue/node_modules/*" "./.idea/*" "./dist/*"
+# In the zip, rename config.prod.php to config.php.
 echo -e '@ config.prod.php\n@=config.php' | zipnote -w dist/plugin.zip
