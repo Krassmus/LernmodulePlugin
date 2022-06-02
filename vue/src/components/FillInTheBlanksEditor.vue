e6d4784ac9abdca613fd389872a3be22eb8fe340
Thomas Hellkamp <thomas.hellkamp@uni-oldenburg.de>
2022-06-02 17:26:26 +0200

    Added delete button and function for templates

--- vue/src/components/FillInTheBlanksEditor.vue
+++ vue/src/components/FillInTheBlanksEditor.vue
@@ -1,13 +1,14 @@
 <template>
   Current undo redo state:
   <pre>{{ currentUndoRedoState }}</pre>
-  <input
-    v-for="(template, index) in taskDefinition.templates"
-    :key="index"
-    type="text"
-    :value="template"
-    @input="(ev) => onEditTemplate(ev, index)"
-  />
+  <template v-for="(template, index) in taskDefinition.templates" :key="index">
+    <input
+      type="text"
+      :value="template"
+      @input="(ev) => onEditTemplate(ev, index)"
+    />
+    <button @click="deleteTemplate(index)">Delete</button>
+  </template>
   <button @click="addSubtask">Add subtask</button>
 </template>
 
@@ -25,6 +26,19 @@ export default defineComponent({
       taskEditorStore.undoRedoStack[taskEditorStore.undoRedoIndex],
   },
   methods: {
+    deleteTemplate(templateIndex: number) {
+      const oldTemplates = this.taskDefinition.templates;
+      const newTemplates = oldTemplates
+        .slice(0, templateIndex)
+        .concat(oldTemplates.slice(templateIndex + 1, oldTemplates.length));
+      taskEditorStore.performEdit({
+        newTaskDefinition: {
+          ...this.taskDefinition,
+          templates: newTemplates,
+        },
+        undoBatch: {},
+      });
+    },
     onEditTemplate(event: InputEvent, templateIndex: number) {
       const newTemplates = [...this.taskDefinition.templates];
       newTemplates[templateIndex] = (event.target as HTMLInputElement).value;
