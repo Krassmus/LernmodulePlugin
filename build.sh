ae031970650c0e901dcfc7f2a052bd5b1dc16483
Ann Yanich <ann@yanich.de>
2022-06-09 14:42:39 +0200

    Put the current timestamp in the build artifact's filename

--- build.sh
+++ build.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 set -o pipefail
-# timestamp=$(date "+%Y.%m.%d-%H.%M.%S")
-
-rm -f dist/plugin.zip &&
+timestamp=$(date "+%Y.%m.%d-%H.%M.%S") &&
+rm -f "dist/*" &&
 cd vue && 
 npm install && 
 npm run build && 
 cd .. &&
 # Zip up the whole plugin directory's contents, excluding certain folders.
 # Stud.IP will take FOREVER to install the plugin if .git or node_modules are included.
-zip -r "dist/plugin.zip" . -x "./.git/*" "./vue/node_modules/*" "./.idea/*" "./dist/*" &&
+zip -r "dist/plugin-$timestamp.zip" . -x "./.git/*" "./vue/node_modules/*" "./.idea/*" "./dist/*" &&
 # In the zip, rename config.prod.php to config.php.
-echo -e '@ config.prod.php\n@=config.php' | zipnote -w dist/plugin.zip
+echo -e '@ config.prod.php\n@=config.php' | zipnote -w "dist/plugin-$timestamp.zip"
