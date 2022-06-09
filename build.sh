925fb7b5a25fceec30e418329ba3fdfaa5e4a689
Ann Yanich <ann@yanich.de>
2022-06-09 14:34:45 +0200

    Rewrite build.sh to handle errors

--- build.sh
+++ build.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-rm -f dist/plugin.zip
-cd vue && npm install && npm run build
-cd ..
-timestamp=$(date "+%Y.%m.%d-%H.%M.%S")
-# Stud.IP takes FOREVER to install the plugin if .git or node_modules are included.
-# zip -r "dist/plugin-$timestamp.zip" . -x "./.git/*" "./vue/node_modules/*" "./.idea/*" "./dist/*"
-zip -r "dist/plugin.zip" . -x "./.git/*" "./vue/node_modules/*" "./.idea/*" "./dist/*"
+set -o pipefail
+# timestamp=$(date "+%Y.%m.%d-%H.%M.%S")
+
+rm -f dist/plugin.zip &&
+cd vue && 
+npm install && 
+npm run build && 
+cd .. &&
+# Zip up the whole plugin directory's contents, excluding certain folders.
+# Stud.IP will take FOREVER to install the plugin if .git or node_modules are included.
+zip -r "dist/plugin.zip" . -x "./.git/*" "./vue/node_modules/*" "./.idea/*" "./dist/*" &&
 # In the zip, rename config.prod.php to config.php.
 echo -e '@ config.prod.php\n@=config.php' | zipnote -w dist/plugin.zip
