29a8bf8b801722ebb5b0bfad525dab453ba9a6ce
Ann Yanich <ann@yanich.de>
2022-06-13 11:45:26 +0200

    Add git hash to plugin zip filename

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -o pipefail
-timestamp=$(date "+%Y.%m.%d-%H.%M.%S") &&
-rm -f "dist/*" &&
+timestamp=$(date "+%Y-%m-%d_%H-%M-%S") &&
+git_hash=$(git rev-parse --short HEAD) &&
 cd vue && 
 npm install && 
 npm run build && 
 cd .. &&
 # Zip up the whole plugin directory's contents, excluding certain folders.
 # Stud.IP will take FOREVER to install the plugin if .git or node_modules are included.
-zip -r "dist/lernmodule-plugin-$timestamp.zip" . -x "./.git/*" "./vue/node_modules/*" "./.idea/*" "./dist/*" &&
+zip -r "dist/lernmodule-plugin-$timestamp-$git_hash.zip" . -x "./.git/*" "./vue/node_modules/*" "./.idea/*" "./dist/*" &&
 # In the zip, rename config.prod.php to config.php.
-echo -e '@ config.prod.php\n@=config.php' | zipnote -w "dist/lernmodule-plugin-$timestamp.zip"
+echo -e '@ config.prod.php\n@=config.php' | zipnote -w "dist/lernmodule-plugin-$timestamp-$git_hash.zip"
