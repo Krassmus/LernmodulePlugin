b09fbcdf3d7563ca775971d8a8f34f76a94f4bdf
Ann Yanich <ann@yanich.de>
2022-07-28 15:13:27 +0200

    Fix build.sh -- remove reference to the deleted files 'config.{dev,prod}.php'

--- build.sh
+++ build.sh
@@ -2,12 +2,10 @@
 set -o pipefail
 timestamp=$(date "+%Y-%m-%d_%H-%M-%S") &&
 git_hash=$(git rev-parse --short HEAD) &&
-cd vue && 
-npm install && 
-npm run build && 
+cd vue &&
+npm install &&
+npm run build &&
 cd .. &&
 # Zip up the whole plugin directory's contents, excluding certain folders.
 # Stud.IP will take FOREVER to install the plugin if .git or node_modules are included.
 zip -r "dist/lernmodule-plugin-$timestamp-$git_hash.zip" . -x "./.git/*" "./vue/node_modules/*" "./.idea/*" "./dist/*" &&
-# In the zip, rename config.prod.php to config.php.
-echo -e '@ config.prod.php\n@=config.php' | zipnote -w "dist/lernmodule-plugin-$timestamp-$git_hash.zip"
