6c4a69a8fa80f87a4922347ad293ee9d7dd0983e
Thomas Hellkamp <thomas.hellkamp@uni-oldenburg.de>
2022-10-03 22:54:53 +0200

    Restyled MultipleChoiceEditor answer area

--- vue/src/components/MultipleChoiceEditor.vue
+++ vue/src/components/MultipleChoiceEditor.vue
@@ -10,8 +10,23 @@
         </label>
         <label v-for="(answer, i) in taskDefinition.answers" :key="i">
           {{ $gettext('Antwort') }}
-          <input type="checkbox" v-model="taskDefinition.answers[i].correct" />
-          <input type="text" v-model="taskDefinition.answers[i].text" />
+          <div class="flex-parent-element">
+            <div class="flex-child-element">
+              <input
+                type="checkbox"
+                v-model="taskDefinition.answers[i].correct"
+                class="checkbox"
+              />
+            </div>
+
+            <div class="flex-child-element">
+              <input
+                type="text"
+                v-model="taskDefinition.answers[i].text"
+                class="textbox"
+              />
+            </div>
+          </div>
         </label>
         <button type="button" @click="addAnswer">
           {{ $gettext('Neue Antwort') }}
@@ -63,4 +78,54 @@ export default defineComponent({
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+.parent {
+  border: 1px solid black;
+  margin: 0rem;
+  padding: 0rem 0rem;
+  text-align: left;
+}
+.child {
+  display: inline-block;
+  border: 1px solid red;
+  padding: 0rem 0rem;
+  vertical-align: middle;
+}
+
+.float-parent-element {
+  width: 50%;
+}
+.float-child-element {
+  float: left;
+  width: 50%;
+}
+
+.flex-parent-element {
+  display: flex;
+  border: 1px solid red;
+  width: 100%;
+  max-width: 48em;
+}
+
+.flex-child-element {
+  flex: 1;
+  border: 1px solid blueviolet;
+  margin: 0px 0px 0px 0px;
+  padding: 0px 0px 0px 0px;
+  /*vertical-align: center;*/
+  /*text-align: center;*/
+}
+
+.flex-child-element:first-child {
+  flex: none;
+}
+
+.checkbox {
+  margin: 11px 1px 0px 0px;
+}
+
+.textbox {
+  margin: 0px 0px 0px 0px;
+  padding: 0px 0px 0px 0px;
+}
+</style>
