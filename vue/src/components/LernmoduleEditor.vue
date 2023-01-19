fe22b924f7c577d9cfae60dc94fa7c35026f1c2c
Thomas Hellkamp <thomas.hellkamp@uni-oldenburg.de>
2023-01-19 11:28:04 +0100

    Cleaned up CSS of LernmoduleEditor.vue and replaced strings with getText

--- vue/src/components/LernmoduleEditor.vue
+++ vue/src/components/LernmoduleEditor.vue
@@ -7,7 +7,7 @@
       @click="saveTask"
       :class="saveStatus.status !== 'saving' ? 'action' : 'action disabled'"
     >
-      <span> Speichern </span>
+      <span>{{ $gettext('Speichern') }}</span>
       <span
         :class="saveStatusText === 'Modified' ? 'save-status-modified' : ''"
         >{{ saveStatusText }}</span
@@ -19,13 +19,13 @@
 
   <form class="default">
     <fieldset>
-      <legend>Grunddaten</legend>
+      <legend>{{ $gettext('Grunddaten') }}</legend>
       <label>
-        Titel
+        {{ $gettext('Titel') }}
         <input type="text" :value="moduleName" @input="onInputModuleName" />
       </label>
       <label>
-        Aufgabenbeschreibung
+        {{ $gettext('Aufgabenbeschreibung') }}
         <textarea
           class="h5pTextArea"
           :value="infotext"
@@ -34,16 +34,16 @@
       </label>
     </fieldset>
   </form>
-  <div>
-    <label>Aufgabentyp auswählen: </label>
+  <div style="margin-bottom: 1em; margin-top: 1em">
+    <label>{{ $gettext('Aufgabentyp auswählen:') }} </label>
     <select :value="taskDefinition.task_type" @input="onSelectTaskType">
-      <option value="FillInTheBlanks">Lückentext</option>
-      <option value="Question">Frage</option>
-      <option value="FlashCards">Karteikarten</option>
-      <option value="DragTheWords">Drag The Words</option>
-      <option value="MarkTheWords">Mark The Words</option>
-      <option value="todo">Hotspot-Frage</option>
-      <option value="todo">Bildersequenz</option>
+      <option value="FillInTheBlanks">{{ $gettext('Lückentext') }}</option>
+      <option value="Question">{{ $gettext('Frage') }}</option>
+      <option value="FlashCards">{{ $gettext('Karteikarten') }}</option>
+      <option value="DragTheWords">{{ $gettext('Drag The Words') }}</option>
+      <option value="MarkTheWords">{{ $gettext('Mark The Words') }}</option>
+      <option value="todo">{{ $gettext('Hotspot-Frage<') }}/option></option>
+      <option value="todo">{{ $gettext('Bildersequenz') }}</option>
     </select>
   </div>
 
@@ -52,15 +52,15 @@
 
     <div class="save-undo-redo">
       <button
-        class="studipButton"
+        class="button"
         @click="saveTask"
         :disabled="saveStatus.status === 'saving'"
       >
-        Speichern
+        {{ $gettext('Speichern') }}
       </button>
     </div>
 
-    <h1 style="margin-top: 1em">Vorschau</h1>
+    <h1 style="margin-top: 1em">{{ $gettext('Vorschau') }}</h1>
     <component
       :is="viewerForTaskType(taskDefinition.task_type)"
       :task="taskDefinition"
@@ -152,15 +152,18 @@ export default defineComponent({
   cursor: pointer;
   color: #28497c;
 }
+
 .action.disabled {
   color: #aaa;
   cursor: not-allowed;
 }
+
 .save-row {
   display: flex;
   justify-content: space-between;
   padding-right: 0.5em;
 }
+
 .save-status-modified {
   font-weight: bold;
 }
@@ -177,39 +180,5 @@ export default defineComponent({
 
 .task-name-input:focus {
   outline: none;
-}
-
-.studipButton {
-  background: #fff;
-  border: 1px solid #28497c;
-  border-radius: 0;
-  -webkit-box-sizing: border-box;
-  box-sizing: border-box;
-  color: #28497c;
-  cursor: pointer;
-  display: inline-block;
-  font-family: Lato, sans-serif;
-  font-size: 14px;
-  line-height: 130%;
-  margin: 0.8em 0.6em 0.8em 0;
-  margin-top: 0.8em;
-  margin-bottom: 0.8em;
-  min-width: 100px;
-  overflow: visible;
-  padding: 5px 15px;
-  position: relative;
-  text-align: center;
-  text-decoration: none;
-  vertical-align: middle;
-  white-space: nowrap;
-  width: auto;
-  -webkit-transition: none;
-  transition: none;
-}
-
-.studipButton:hover {
-  background: #28497c;
-  color: #fff;
-  outline: 0;
 }
 </style>
