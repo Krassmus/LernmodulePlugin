e76ee52406937506d0adc83d953e84e46190de86
Thomas Hellkamp <thomas.hellkamp@uni-oldenburg.de>
2023-01-19 11:35:07 +0100

    Cleaned up CSS and HTML of editor for question task

--- vue/src/components/QuestionEditor.vue
+++ vue/src/components/QuestionEditor.vue
@@ -1,92 +1,88 @@
 <template>
   <!--  The current task: {{ task }}-->
-  <div>
-    <form class="default">
-      <fieldset>
-        <legend>{{ $gettext('Frage') }}</legend>
-        <label>
-          {{ $gettext('Frage') }}
-          <input type="text" v-model="taskDefinition.question" />
-        </label>
-        <label v-for="(answer, i) in taskDefinition.answers" :key="i">
-          {{ $gettext('Antwort') }} {{ i + 1 }}
-          <div class="flex-parent-element">
-            <input
-              class="flex-child-element, checkbox"
-              type="checkbox"
-              v-model="taskDefinition.answers[i].correct"
-            />
-            <input
-              class="flex-child-element, textbox"
-              type="text"
-              v-model="taskDefinition.answers[i].text"
-            />
-            <img
-              class="flex-child-element, removeAnswerButton"
-              :src="urlForIcon('remove-circle')"
-              @click="removeAnswer(answer)"
-            />
-          </div>
-        </label>
-        <button type="button" @click="addAnswer">
-          {{ $gettext('Neue Antwort') }}
-        </button>
-      </fieldset>
-      <fieldset class="collapsable">
-        <legend>{{ $gettext('Einstellungen') }}</legend>
-        <h1>{{ $gettext('Generell') }}</h1>
-        <label>
-          <input type="checkbox" v-model="taskDefinition.canAnswerMultiple" />
-          {{
-            $gettext('Mehrere Antworten können gleichzeitig ausgewählt werden')
-          }}
-        </label>
-
-        <label>
-          <input type="checkbox" v-model="taskDefinition.randomOrder" />
-          {{ $gettext('Zeige Antworten in zufälliger Reihenfolge') }}
-        </label>
-
-        <h1>{{ $gettext('Versuche') }}</h1>
-        <label>
-          <input type="checkbox" v-model="taskDefinition.retryAllowed" />
-          {{ $gettext('Mehrere Versuche erlauben') }}
-        </label>
-        <label
-          :class="
-            taskDefinition.retryAllowed ? '' : 'h5pBehaviorSetting-disabled'
-          "
-          >{{ $gettext('Text im Button:') }}
-
+  <form class="default">
+    <fieldset>
+      <legend>{{ $gettext('Frage') }}</legend>
+      <label>
+        {{ $gettext('Frage') }}
+        <input type="text" v-model="taskDefinition.question" />
+      </label>
+      <label v-for="(answer, i) in taskDefinition.answers" :key="i">
+        {{ $gettext('Antwort') }} {{ i + 1 }}
+        <div class="flex-parent-element">
           <input
-            type="text"
-            :disabled="!taskDefinition.retryAllowed"
-            v-model="taskDefinition.strings.retryButton"
-        /></label>
-        <h1>{{ $gettext('Lösungen') }}</h1>
-        <label>
-          <input
+            class="flex-child-element, checkbox"
             type="checkbox"
-            v-model="taskDefinition.showSolutionsAllowed"
+            v-model="taskDefinition.answers[i].correct"
           />
-          {{ $gettext('Lösungen können angezeigt werden') }}
-        </label>
-        <label
-          :class="
-            taskDefinition.showSolutionsAllowed
-              ? ''
-              : 'h5pBehaviorSetting-disabled'
-          "
-          >{{ $gettext('Text im Button:') }}
-
           <input
+            class="flex-child-element, textbox"
             type="text"
-            :disabled="!taskDefinition.showSolutionsAllowed"
-            v-model="taskDefinition.strings.solutionsButton"
-        /></label>
-      </fieldset>
-    </form>
-  </div>
+            v-model="taskDefinition.answers[i].text"
+          />
+          <img
+            class="flex-child-element, removeAnswerButton"
+            :src="urlForIcon('remove-circle')"
+            @click="removeAnswer(answer)"
+          />
+        </div>
+      </label>
+      <button type="button" class="button" @click="addAnswer">
+        {{ $gettext('Neue Antwort') }}
+      </button>
+    </fieldset>
+
+    <fieldset class="collapsable">
+      <legend>{{ $gettext('Einstellungen') }}</legend>
+      <h1>{{ $gettext('Generell') }}</h1>
+      <label>
+        <input type="checkbox" v-model="taskDefinition.canAnswerMultiple" />
+        {{
+          $gettext('Mehrere Antworten können gleichzeitig ausgewählt werden')
+        }}
+      </label>
+
+      <label>
+        <input type="checkbox" v-model="taskDefinition.randomOrder" />
+        {{ $gettext('Zeige Antworten in zufälliger Reihenfolge') }}
+      </label>
+
+      <h1>{{ $gettext('Versuche') }}</h1>
+      <label>
+        <input type="checkbox" v-model="taskDefinition.retryAllowed" />
+        {{ $gettext('Mehrere Versuche erlauben') }}
+      </label>
+      <label
+        :class="
+          taskDefinition.retryAllowed ? '' : 'h5pBehaviorSetting-disabled'
+        "
+        >{{ $gettext('Text im Button:') }}
+
+        <input
+          type="text"
+          :disabled="!taskDefinition.retryAllowed"
+          v-model="taskDefinition.strings.retryButton"
+      /></label>
+      <h1>{{ $gettext('Lösungen') }}</h1>
+      <label>
+        <input type="checkbox" v-model="taskDefinition.showSolutionsAllowed" />
+        {{ $gettext('Lösungen können angezeigt werden') }}
+      </label>
+      <label
+        :class="
+          taskDefinition.showSolutionsAllowed
+            ? ''
+            : 'h5pBehaviorSetting-disabled'
+        "
+        >{{ $gettext('Text im Button:') }}
+
+        <input
+          type="text"
+          :disabled="!taskDefinition.showSolutionsAllowed"
+          v-model="taskDefinition.strings.solutionsButton"
+      /></label>
+    </fieldset>
+  </form>
 </template>
 
 <script lang="ts">
@@ -141,27 +137,14 @@ export default defineComponent({
 /*  border: 4px solid #1d75b3;*/
 /*}*/
 
-/*.flex-child-element:first-child {*/
-/*  flex: none;*/
+/*input[type='text'].textbox.textbox {*/
+/*  margin: 0px 0px 0px 0px;*/
+/*  padding: 0px 0px 0px 0px;*/
 /*}*/
 
-/*.flex-child-element:last-child {*/
-/*  flex: none;*/
-/*}*/
-
-input[type='text'].textbox.textbox {
-  margin: 0px 0px 0px 0px;
-  padding: 0px 0px 0px 0px;
-}
-
-input[type='checkbox'].checkbox.checkbox {
-  margin: 0px 0px 0px 0px;
-  padding: 0px 0px 0px 0px;
-}
-
-/*button[type='button'] {*/
-/*  margin: 10px 0px 0px 0px;*/
-/*  padding: 10px 0px 0px 0px;*/
+/*input[type='checkbox'].checkbox.checkbox {*/
+/* margin: 0px 0px 0px 0px;*/
+/* padding: 0px 0px 0px 0px;*/
 /*}*/
 
 .h5pBehaviorSetting-disabled {
