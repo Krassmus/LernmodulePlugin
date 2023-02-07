72ee0c19f8bcea0a5587c2faf2ad31212f2f548a
Thomas Hellkamp <thomas.hellkamp@uni-oldenburg.de>
2023-02-07 22:49:25 +0100

    Added global.css import to viewer-main.ts

--- vue/src/viewer-main.ts
+++ vue/src/viewer-main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import LernmoduleViewer from '@/components/LernmoduleViewer.vue';
 import { gettextPlugin } from '@/language/gettext';
+import './assets/global.css';
 
 const app = createApp(LernmoduleViewer);
 app.use(gettextPlugin);
