2deaa062581a5fabeccc85d7a7c36ffadeb25537
Thomas Hellkamp <thomas.hellkamp@uni-oldenburg.de>
2023-05-23 14:36:00 +0200

    Removed animation example in Question task

--- vue/src/components/QuestionViewer.vue
+++ vue/src/components/QuestionViewer.vue
@@ -66,10 +66,6 @@
         width="36"
         height="36"
       />
-      <div :class="{ shake: shaking }">
-        <button @click="shakeit">Click me</button>
-        <span v-if="shaking">This feature is disabled!</span>
-      </div>
     </div>
 
     <button
@@ -295,33 +291,5 @@ button {
 .flex-child:nth-of-type(2) {
   //background: #a2d4d8;
   flex-grow: 0;
-}
-
-.shake {
-  animation: shake 0.82s cubic-bezier(0.36, 0.07, 0.19, 0.97) both;
-  transform: translate3d(0, 0, 0);
-}
-
-@keyframes shake {
-  10%,
-  90% {
-    transform: translate3d(-1px, 0, 0);
-  }
-
-  20%,
-  80% {
-    transform: translate3d(2px, 0, 0);
-  }
-
-  30%,
-  50%,
-  70% {
-    transform: translate3d(-4px, 0, 0);
-  }
-
-  40%,
-  60% {
-    transform: translate3d(4px, 0, 0);
-  }
 }
 </style>
