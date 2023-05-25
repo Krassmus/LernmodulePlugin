de1dbc08878ee5f24cab5dbce751f239e36c7b28
Thomas Hellkamp <thomas.hellkamp@uni-oldenburg.de>
2023-05-25 10:07:11 +0200

    Added new FeedbackElement to be used across multiple tasks

---
+++ vue/src/components/FeedbackElement.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="h5pFeedbackContainer">
+    <div class="h5pFeedbackContainerTop">
+      <div v-if="message" class="h5pFeedbackText">
+        {{ message }}
+      </div>
+      <meter id="score" min="0" :max="maxPoints" :value="achievedPoints" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+// need v-model to provide and get content -> <studip-wysiwyg v-model="content" />
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'feedback-element',
+  props: {
+    message: String,
+    maxPoints: Number,
+    achievedPoints: Number,
+  },
+  data() {
+    return {};
+  },
+  mounted() {},
+  methods: {},
+});
+</script>
+
+<style></style>
