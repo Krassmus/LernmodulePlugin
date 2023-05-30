c3b53302d3adfa7b6b8739cadf7ff82398bfe0af
Ann Yanich <ann.yanich@uol.de>
2023-05-30 13:31:35 +0200

    Define CoursewareController with editor_action and corresponding view template

---
+++ controllers/courseware.php
@@ -0,0 +1,9 @@
+<?php
+
+class CoursewareController extends PluginController
+{
+    public function editor_action()
+    {
+        $this->render_template('courseware/editor');
+    }
+}
---
+++ views/courseware/editor.php
@@ -0,0 +1,1 @@
+<div>This is where the Vue 3 code should be included</div>
