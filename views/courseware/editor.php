d743a7483ba971e8f03354b4b2a8d6929cd8f55d
Ann Yanich <ann.yanich@uol.de>
2023-05-30 17:20:44 +0200

    Add !important to css rule removing the padding from #layout_content in the courseware/editor view template

--- views/courseware/editor.php
+++ views/courseware/editor.php
@@ -1,4 +1,7 @@
-<div>This is where the Vue 3 code for the Lernmodule Courseware Block should be included</div>
+<!-- This is the template for the iframe where our Vue 3 code is embedded inside of our Vue 2 Courseware block component.
+  In the future, this can be refactored and removed when Courseware
+  is finally updated to Vue 3. -->
+
 <!--  Hide all of Stud.IP's layout elements and remove extraneous padding.-->
 <style>
     #barBottomContainer, #flex-header, .secondary-navigation, #layout-sidebar, #layout_footer, #page_title_container {
@@ -6,17 +9,17 @@
     }
 
     #layout_wrapper, #layout_content, #layout_container {
-        padding: 0;
+        padding: 0 !important;
     }
 </style>
 
 <div id="app">
-    <h1>Lernmodule Courseware Block Template</h1>
-    Wenn die Vue-App nach Laden der Seite an dieser Stelle nicht erscheint, kann es sein, dass der
-    Dev-Server nicht läuft oder der Production-Build nicht ausgeführt wurde.
-    <pre><?= Config::get()->LERNMODULE_DEBUG ?
-            'LERNMODULE_DEBUG === true. Überprüfe, ob der Dev-Server läuft.' :
-            'LERNMODULE_DEBUG === false. Überprüfe die Inhalte von /vue/dist.' ?></pre>
+  <h1>Lernmodule Courseware Block Template</h1>
+  Wenn die Vue-App nach Laden der Seite an dieser Stelle nicht erscheint, kann es sein, dass der
+  Dev-Server nicht läuft oder der Production-Build nicht ausgeführt wurde.
+  <pre><?= Config::get()->LERNMODULE_DEBUG ?
+          'LERNMODULE_DEBUG === true. Überprüfe, ob der Dev-Server läuft.' :
+          'LERNMODULE_DEBUG === false. Überprüfe die Inhalte von /vue/dist.' ?></pre>
 </div>
 
 <script>
@@ -24,13 +27,13 @@
 </script>
 
 <?php if (Config::get()->LERNMODULE_DEBUG) : ?>
-    <script src="http://localhost:8080/js/courseware.js"></script>
-    <script src="http://localhost:8080/js/chunk-vendors.js"></script>
-    <script src="http://localhost:8080/js/chunk-common.js"></script>
+  <script src="http://localhost:8080/js/courseware.js"></script>
+  <script src="http://localhost:8080/js/chunk-vendors.js"></script>
+  <script src="http://localhost:8080/js/chunk-common.js"></script>
 <?php else : ?>
-    <script src="<?= $plugin->getPluginUrl() . '/vue/dist/js/courseware.js' ?>"></script>
-    <script src="<?= $plugin->getPluginUrl() . '/vue/dist/js/chunk-vendors.js' ?>"></script>
-    <script src="<?= $plugin->getPluginUrl() . '/vue/dist/js/chunk-common.js' ?>"></script>
-    <link rel="stylesheet"
-          href="<?= $plugin->getPluginUrl() . '/vue/dist/css/courseware.css' ?>"></link>
+  <script src="<?= $plugin->getPluginUrl() . '/vue/dist/js/courseware.js' ?>"></script>
+  <script src="<?= $plugin->getPluginUrl() . '/vue/dist/js/chunk-vendors.js' ?>"></script>
+  <script src="<?= $plugin->getPluginUrl() . '/vue/dist/js/chunk-common.js' ?>"></script>
+  <link rel="stylesheet"
+        href="<?= $plugin->getPluginUrl() . '/vue/dist/css/courseware.css' ?>"></link>
 <?php endif ?>
