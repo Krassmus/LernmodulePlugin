e0d0cf4e122e488852927fa44ca35bbfbfecaa8d
Thomas Hellkamp <thomas.hellkamp@uni-oldenburg.de>
2023-07-31 15:21:54 +0200

    Implemented the basic drag and drop functionality in Image Pairing task

--- vue/src/components/ImagePairingViewer.vue
+++ vue/src/components/ImagePairingViewer.vue
@@ -1,60 +1,62 @@
 <template>
-  <div class="imagePairGrid">
-    <div class="imagePairColumn">
-      <div
-        v-for="imagePair in this.task.imagePairs"
-        :key="imagePair.uuid"
-        class="imagePairItem"
-        draggable="true"
-        @dragstart="startDragImage($event, imagePair.draggableImage)"
-        @drop="onDropImage($event, imagePair.draggableImage)"
-        @dragover.prevent
-        @dragenter.prevent
+  <div class="imagePairingRow">
+    <div class="draggableImagesColumn">
+      <template
+        v-for="draggableImage in Object.keys(this.draggableImages)"
+        :key="draggableImage"
       >
         <img
-          :src="imagePair.draggableImage.imageUrl"
-          :alt="imagePair.draggableImage.altText"
-          class="pairImage"
+          :class="getClassForDraggableImage(draggableImage)"
+          draggable="true"
+          @dragstart="startDragImage($event, draggableImage)"
+          :src="getImageById(draggableImage).imageUrl"
+          :alt="getImageById(draggableImage).altText"
         />
-      </div>
+      </template>
     </div>
 
-    <div class="imagePairColumn">
-      <div
-        v-for="imagePair in this.task.imagePairs"
+    <div class="imagePairsColumn">
+      <ViewerImagePair
+        v-for="imagePair in this.userLinkedImagePairs"
+        :draggable-image="getImageByIdWithUndefined(imagePair.draggableImageId)"
+        :target-image="getImageById(imagePair.targetImageId)"
         :key="imagePair.uuid"
-        class="imagePairItem"
+        @drop="onDropImage($event, imagePair)"
         draggable="true"
-        @dragstart="startDragImage($event, imagePair.targetImage)"
-        @drop="onDropImage($event, imagePair.targetImage)"
+        @dragstart="startDragImagePair($event, imagePair)"
         @dragover.prevent
         @dragenter.prevent
-      >
-        <img
-          :src="imagePair.targetImage.imageUrl"
-          :alt="imagePair.targetImage.altText"
-          class="pairImage"
-        />
-      </div>
+        @click="onClickImagePair(imagePair)"
+      />
     </div>
   </div>
-  <pre>{{ draggedImageId }}</pre>
 </template>
 
 <script lang="ts">
 import { defineComponent, PropType } from 'vue';
-import { ImagePairingTask } from '@/models/TaskDefinition';
-
-interface Image {
-  uuid: Uuid;
-  imageUrl: string;
-  altText: string;
-}
+import {
+  Image,
+  ImagePair,
+  ImagePairingTask,
+  MemoryCard,
+} from '@/models/TaskDefinition';
+import { v4 } from 'uuid';
+import ViewerImagePair from '@/components/ViewerImagePair.vue';
 
 type Uuid = string;
 
+export interface UserLinkedImagePair {
+  uuid: Uuid;
+  draggableImageId: Uuid | undefined;
+  correctDraggableImageId: Uuid;
+  targetImageId: Uuid;
+}
+
 export default defineComponent({
   name: 'ImagePairingViewer',
+  components: {
+    ViewerImagePair,
+  },
   props: {
     task: {
       type: Object as PropType<ImagePairingTask>,
@@ -64,56 +66,158 @@ export default defineComponent({
   data() {
     return {
       draggedImageId: undefined as Uuid | undefined,
+      images: [] as Image[],
+      draggableImages: {} as Record<Uuid, boolean>,
+      userLinkedImagePairs: [] as UserLinkedImagePair[],
     };
   },
   methods: {
-    startDragImage(dragEvent: DragEvent, image: Image): void {
+    startDragImage(dragEvent: DragEvent, imageId: Uuid): void {
       if (dragEvent.dataTransfer) {
         dragEvent.dataTransfer.dropEffect = 'move';
         dragEvent.dataTransfer.effectAllowed = 'move';
 
-        console.log('Dragging image: ', image);
-        this.draggedImageId = image.uuid;
+        if (!this.draggableImages[imageId]) {
+          console.log('Dragging image:', imageId);
+          this.draggedImageId = imageId;
+        }
       }
     },
 
-    onDropImage(dragEvent: DragEvent, image: Image): void {
+    startDragImagePair(
+      dragEvent: DragEvent,
+      imagePair: UserLinkedImagePair
+    ): void {
+      if (dragEvent.dataTransfer) {
+        dragEvent.dataTransfer.dropEffect = 'move';
+        dragEvent.dataTransfer.effectAllowed = 'move';
+
+        if (imagePair.draggableImageId !== undefined) {
+          this.draggedImageId = imagePair.draggableImageId;
+
+          console.log(
+            'Dragging image:',
+            this.draggedImageId,
+            'from image pair',
+            imagePair.uuid
+          );
+        }
+      }
+    },
+
+    onDropImage(event: DragEvent, targetImagePair: UserLinkedImagePair): void {
       if (!this.draggedImageId) {
-        throw new Error('Dragged image id is undefined');
+        return;
+      }
+
+      if (targetImagePair.draggableImageId === undefined) {
+        const originImagePair = this.userLinkedImagePairs.find(
+          (imagePair) => imagePair.draggableImageId === this.draggedImageId
+        );
+
+        if (originImagePair !== undefined) {
+          originImagePair.draggableImageId = undefined;
+        }
+
+        targetImagePair.draggableImageId = this.draggedImageId;
+
+        this.draggableImages[this.draggedImageId] = true;
       }
 
       console.log(
-        'Dropped image :',
+        'Dropped image:',
         this.draggedImageId,
-        'on image: ',
-        image.altText
+        'on image pair:',
+        targetImagePair.uuid
       );
 
       this.draggedImageId = undefined;
     },
+
+    getImageById(imageId: string): Image {
+      const image = this.images.find((image) => image.uuid === imageId);
+      if (!image) {
+        throw new Error('No image found with the given ID: ' + imageId);
+      }
+      return image;
+    },
+
+    getImageByIdWithUndefined(imageId: string | undefined): Image | undefined {
+      if (imageId === undefined) return undefined;
+      const image = this.images.find((image) => image.uuid === imageId);
+      if (!image) {
+        throw new Error('No image found with the given ID: ' + imageId);
+      }
+      return image;
+    },
+
+    onClickImagePair(imagePair: UserLinkedImagePair): void {
+      console.log('Clicked on image pair', imagePair.uuid);
+      if (imagePair.draggableImageId !== undefined) {
+        this.draggableImages[imagePair.draggableImageId] = false;
+        imagePair.draggableImageId = undefined;
+      }
+    },
+
+    getClassForDraggableImage(draggableImageId: Uuid) {
+      if (this.draggableImages[draggableImageId]) {
+        return 'draggableImageHidden';
+      } else {
+        return 'draggableImage';
+      }
+    },
   },
   computed: {},
+  watch: {
+    task: {
+      handler() {
+        console.log('watcher for this.task');
+        // Make two copies of each card in the task. Add the flipped, solved and matchingCardId attributes
+
+        this.images = [];
+        this.userLinkedImagePairs = [];
+
+        for (const imagePair of this.task.imagePairs) {
+          this.images.push(imagePair.draggableImage);
+          this.images.push(imagePair.targetImage);
+
+          this.draggableImages[imagePair.draggableImage.uuid] = false;
+
+          this.userLinkedImagePairs.push({
+            uuid: imagePair.uuid,
+            draggableImageId: undefined,
+            correctDraggableImageId: imagePair.draggableImage.uuid,
+            targetImageId: imagePair.targetImage.uuid,
+          });
+        }
+      },
+      immediate: true, // Ensure that the watcher is also called immediately when the component is first mounted
+    },
+  },
 });
 </script>
 
 <style scoped>
-.pairImage {
-  max-width: 100%;
-  max-height: 100%;
-}
-
-.imagePairGrid {
+.imagePairingRow {
   display: flex;
   flex-direction: row;
 }
 
-.imagePairColumn {
+.draggableImagesColumn {
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
+  flex-wrap: wrap;
   align-items: center;
 }
 
-.imagePairItem {
+.imagePairsColumn {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
+}
+
+.draggableImage {
   display: flex;
   justify-content: center;
   width: 10em;
@@ -126,7 +230,21 @@ export default defineComponent({
   box-shadow: 2px 2px 0 2px rgba(203, 213, 222, 0.2);
 }
 
-.imagePairItem:hover {
+.draggableImageHidden {
+  display: flex;
+  justify-content: center;
+  width: 10em;
+  padding: 6px;
+  margin: 6px;
+  height: 10em;
+  border-radius: 6px 6px 6px 6px;
+  cursor: pointer;
+  border: 2px solid #dbe2e8;
+  box-shadow: 2px 2px 0 2px rgba(203, 213, 222, 0.2);
+  opacity: 20%;
+}
+
+.draggableImage:hover {
   border: 2px solid rgba(0, 187, 109, 0.93);
 }
 </style>
---
+++ vue/src/components/ViewerImagePair.vue
@@ -0,0 +1,84 @@
+<template>
+  <div v-if="draggableImage" class="twoImages">
+    <img
+      :src="draggableImage.imageUrl"
+      :alt="draggableImage.altText"
+      class="smallImage"
+    />
+
+    <img
+      :src="targetImage.imageUrl"
+      :alt="targetImage.altText"
+      class="smallImage"
+    />
+  </div>
+  <div v-else class="oneImage">
+    <img
+      :src="targetImage.imageUrl"
+      :alt="targetImage.altText"
+      class="bigImage"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import { Image } from '@/models/TaskDefinition';
+
+export default defineComponent({
+  name: 'ViewerImagePair',
+  components: {},
+  props: {
+    draggableImage: {
+      type: Object as PropType<Image>,
+      required: false,
+    },
+    targetImage: {
+      type: Object as PropType<Image>,
+      required: true,
+    },
+  },
+  computed: {},
+  methods: {},
+});
+</script>
+
+<style scoped>
+.smallImage {
+  width: 50%;
+  height: 50%;
+}
+
+.bigImage {
+  width: 100%;
+  height: 100%;
+}
+
+.oneImage {
+  display: flex;
+  justify-content: center;
+  width: 10em;
+  padding: 6px;
+  margin: 6px;
+  height: 10em;
+  border-radius: 6px 6px 6px 6px;
+  cursor: pointer;
+  border: 2px solid #dbe2e8;
+  box-shadow: 2px 2px 0 2px rgba(203, 213, 222, 0.2);
+}
+
+.twoImages {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 10em;
+  padding: 6px;
+  margin: 6px;
+  height: 10em;
+  border-radius: 6px 6px 6px 6px;
+  cursor: pointer;
+  border: 2px solid #dbe2e8;
+  box-shadow: 2px 2px 0 2px rgba(203, 213, 222, 0.2);
+}
+</style>
