7b3afe7e57c485a0a1765566b2b46081f41a3572
Thomas Hellkamp <thomas.hellkamp@uni-oldenburg.de>
2023-10-12 19:15:39 +0200

    Added Image Sequencing task

---
+++ vue/src/components/ImageSequencingEditor.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="main-flex">Image Sequencing Editor</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import { ImageSequencingTask } from '@/models/TaskDefinition';
+import { $gettext } from '@/language/gettext';
+import { taskEditorStore } from '@/store';
+
+export default defineComponent({
+  name: 'ImageSequencingEditor',
+  components: {},
+  props: {
+    task: {
+      type: Object as PropType<ImageSequencingTask>,
+      required: true,
+    },
+  },
+  data() {
+    return {};
+  },
+  beforeMount(): void {},
+  methods: {
+    $gettext,
+    urlForIcon(iconName: string) {
+      return (
+        window.STUDIP.ASSETS_URL + 'images/icons/blue/' + iconName + '.svg'
+      );
+    },
+  },
+  computed: {
+    taskDefinition: () => taskEditorStore.taskDefinition as ImageSequencingTask,
+  },
+});
+</script>
+
+<style scoped>
+.main-flex {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  gap: 1em;
+}
+</style>
