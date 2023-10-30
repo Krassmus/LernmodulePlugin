a7f7667d009ef176cd127681ee381b244199a519
Ann Yanich <ann.yanich@uol.de>
2023-10-30 13:08:47 -0700

    Make timeline axis using html/css and vue3

--- vue/src/components/interactiveVideo/VideoTimeline.vue
+++ vue/src/components/interactiveVideo/VideoTimeline.vue
@@ -1,20 +1,3 @@
-<template>
-  <div class="timeline-root">
-    <div class="timeline">
-      The timeline.
-      <div>
-        Length:
-        <pre>{{ videoMetadata.length }}</pre>
-      </div>
-    </div>
-    <div
-      class="time-marker"
-      :style="{
-        left: positionForTimeMarker,
-      }"
-    />
-  </div>
-</template>
 <script lang="ts">
 import { defineComponent, PropType } from 'vue';
 import { VideoMetadata } from '@/components/interactiveVideo/events';
@@ -35,17 +18,95 @@ export default defineComponent({
     positionForTimeMarker(): string {
       return `${(this.currentTime / this.videoMetadata.length) * 100}%`;
     },
+    axisScale(): number[] {
+      const points = 26;
+      const start = 0;
+      const end = this.videoMetadata.length;
+      const scale: number[] = [];
+      for (let i = 0; i < points; i++) {
+        scale.push(start + ((end - start) / points) * i);
+      }
+      return scale;
+    },
+  },
+  methods: {
+    // mm:ss
+    formatVideoTimestamp(seconds: number): string {
+      const date = new Date(0);
+      date.setSeconds(seconds);
+      const timeString = date.toISOString().substring(14, 19);
+      return timeString;
+    },
   },
 });
 </script>
-<style scoped>
+
+<template>
+  <div class="timeline-root">
+    <div class="timeline-axis">
+      <div class="tick" v-for="(point, index) in axisScale" :key="index">
+        <div class="tick-label">
+          {{ index % 5 === 0 ? formatVideoTimestamp(point) : ' ' }}
+        </div>
+        <div
+          class="tick-line"
+          :class="{
+            major: index % 5 === 0,
+          }"
+        ></div>
+      </div>
+    </div>
+    <div class="timeline">
+      The timeline.
+      <div>
+        Length:
+        <pre>{{ videoMetadata.length }}</pre>
+      </div>
+    </div>
+    <div
+      class="time-marker"
+      :style="{
+        left: positionForTimeMarker,
+      }"
+    />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.timeline-root {
+  position: relative;
+  margin-left: 1em;
+  margin-right: 1em;
+}
+.timeline-axis {
+  top: 0;
+  display: flex;
+  justify-content: space-between;
+  .tick {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    width: 1em;
+    .tick-label {
+      white-space: pre;
+      align-self: start;
+    }
+    .tick-line {
+      align-self: start;
+      width: 1px;
+      border-left: thin solid lightgray;
+      height: 1em;
+      &.major {
+        height: 1.5em;
+      }
+    }
+  }
+}
 .timeline {
   width: 100%;
   height: 5em;
   border: 1px solid black;
-}
-.timeline-root {
-  position: relative;
 }
 .time-marker {
   position: absolute;
