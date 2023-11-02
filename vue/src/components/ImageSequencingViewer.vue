db2f2dad55755f36195a026db032dc952302de8e
Thomas Hellkamp <thomas.hellkamp@uni-oldenburg.de>
2023-11-02 12:28:28 +0100

    Image Sequencing: Added basic structure and look in viewer

--- vue/src/components/ImageSequencingViewer.vue
+++ vue/src/components/ImageSequencingViewer.vue
@@ -1,10 +1,27 @@
 <template>
-  <div class="main-flex">Image Sequencing Viewer</div>
+  <div class="imageRow">
+    <div
+      v-for="image in images"
+      :key="image.uuid"
+      class="imageContainer"
+      @dragover.prevent
+      @dragenter.prevent
+    >
+      <img
+        class="draggableImage"
+        :src="image.imageUrl"
+        :alt="image.altText"
+        draggable="true"
+        @dragstart="startDragImage($event, image)"
+        @drop="onDropImage($event, image)"
+      />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, PropType } from 'vue';
-import { ImageSequencingTask } from '@/models/TaskDefinition';
+import { Image, ImageSequencingTask } from '@/models/TaskDefinition';
 import { $gettext } from '@/language/gettext';
 import { taskEditorStore } from '@/store';
 
@@ -18,7 +35,9 @@ export default defineComponent({
     },
   },
   data() {
-    return {};
+    return {
+      imageInteractedWith: undefined as Image | undefined,
+    };
   },
   beforeMount(): void {},
   methods: {
@@ -28,9 +47,30 @@ export default defineComponent({
         window.STUDIP.ASSETS_URL + 'images/icons/blue/' + iconName + '.svg'
       );
     },
+    startDragImage(dragEvent: DragEvent, image: Image) {
+      this.imageInteractedWith = image;
+      console.log('Dragging image', image.altText);
+    },
+
+    onDropImage(event: DragEvent, image: Image): void {
+      console.log(
+        'Dropped image',
+        this.imageInteractedWith?.altText,
+        'on target:',
+        image.altText
+      );
+    },
   },
   computed: {
     taskDefinition: () => taskEditorStore.taskDefinition as ImageSequencingTask,
+    images(): Image[] {
+      // Shuffle the images
+      // https://stackoverflow.com/a/46545530
+      return this.task.images
+        .map((image) => ({ image: image, sort: Math.random() }))
+        .sort((image1, image2) => image1.sort - image2.sort)
+        .map(({ image }) => image);
+    },
   },
 });
 </script>
@@ -41,5 +81,22 @@ export default defineComponent({
   justify-content: space-between;
   width: 100%;
   gap: 1em;
+}
+
+.imageRow {
+  display: flex;
+  flex-direction: row;
+}
+
+.imageContainer {
+  border: 2px solid #dbe2e8;
+  border-radius: 6px;
+  margin: 6px;
+  padding: 6px;
+}
+
+.draggableImage {
+  width: 10em;
+  height: 10em;
 }
 </style>
