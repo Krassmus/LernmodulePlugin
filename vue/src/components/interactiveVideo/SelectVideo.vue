315cc52c9c3a4fc11e77eb68e175815f1729b607
Ann Yanich <ann.yanich@uol.de>
2024-01-10 14:33:07 +0100

    WIP implement HH:MM:SS time input

--- vue/src/components/interactiveVideo/SelectVideo.vue
+++ vue/src/components/interactiveVideo/SelectVideo.vue
@@ -8,6 +8,28 @@ import { InteractiveVideoTask } from '@/models/InteractiveVideoTask';
 import FileUpload from '@/components/FileUpload.vue';
 import { UploadedFile } from '@/routes';
 
+function formatSecondsToHhMmSs(time: number): string {
+  let hours = 0,
+    minutes = 0,
+    seconds = 0;
+  while (time > 3600) {
+    time -= 3600;
+    hours += 1;
+  }
+  while (time > 60) {
+    time -= 60;
+    minutes += 1;
+  }
+  seconds = time;
+  function twoDigits(n: number): string {
+    return n.toLocaleString('de-DE', {
+      minimumIntegerDigits: 2,
+      maximumFractionDigits: 0,
+    });
+  }
+  return `${twoDigits(hours)}:${twoDigits(minutes)}:${twoDigits(seconds)}`;
+}
+
 export default defineComponent({
   name: 'SelectVideo',
   props: {
@@ -19,8 +41,42 @@ export default defineComponent({
   data() {
     return {
       youtubeUrlInput: '',
+      startAtInput: '',
       currentTime: 0,
     };
+  },
+  computed: {
+    /**
+     * Either the time the user input, converted into seconds, or an Error with
+     * a message explaining why the user's input was invalid.
+     */
+    parsedStartAtInput(): number | Error {
+      // This regex is adapted from https://stackoverflow.com/a/8318367/7359454
+      // License CC BY-SA 3.0 https://creativecommons.org/licenses/by-sa/3.0/
+      // Accessed and adapted on 10.01.2024 by Ann Yanich
+      const timeRegex = /^(?:(?:([0-9]?\d):)?([0-5]?\d):)?([0-5]?\d)$/;
+      const match = timeRegex.exec(this.startAtInput);
+      if (!match || match.length !== 4) {
+        return new Error(
+          $gettext('Die Startposition muss der Syntax HH:MM:SS entsprechen.')
+        );
+      }
+      // match[0] is the time string with colons,
+      // match[1] is hours (or empty string if not present),
+      // match[2] is minutes (or empty string if not present),
+      // and match[3] is seconds (always present).
+      if (match[1] !== '') {
+        return (
+          parseInt(match[1]) * 3600 +
+          parseInt(match[2]) * 60 +
+          parseInt(match[3])
+        );
+      } else if (match[2] !== '') {
+        return parseInt(match[2]) * 60 + parseInt(match[3]);
+      } else {
+        return parseInt(match[3]);
+      }
+    },
   },
   watch: {
     'taskDefinition.video': {
@@ -33,10 +89,14 @@ export default defineComponent({
         }
       },
     },
-    'taskDefinition.startAt': function (startAt: number) {
-      (
-        this.$refs.videoPlayer as InstanceType<typeof VideoPlayer>
-      )?.player!.currentTime(startAt);
+    'taskDefinition.startAt': {
+      immediate: true,
+      handler: function (startPosition: number) {
+        (
+          this.$refs.videoPlayer as InstanceType<typeof VideoPlayer>
+        )?.player!.currentTime(startPosition);
+        this.startAtInput = formatSecondsToHhMmSs(startPosition);
+      },
     },
   },
   methods: {
@@ -46,6 +106,15 @@ export default defineComponent({
     },
     onClickUseCurrentTime() {
       this.taskDefinition.startAt = this.currentTime;
+    },
+    onBlurStartPositionInput() {
+      if (this.parsedStartAtInput instanceof Error) {
+        // Reset the input to a good value
+        this.startAtInput = formatSecondsToHhMmSs(this.taskDefinition.startAt);
+      } else {
+        // Save the value into the task definition
+        this.taskDefinition.startAt = this.parsedStartAtInput;
+      }
     },
     onSaveYoutubeVideo() {
       this.taskDefinition.video = {
@@ -151,17 +220,20 @@ export default defineComponent({
         {{ $gettext('Automatisch abspielen') }}
       </label>
       <div class="start-at-setting">
-        <label>
-          {{ $gettext('Anfangen um') }}
-          <input
-            type="number"
-            class="wide"
-            v-model.number="taskDefinition.startAt"
-          />
-        </label>
-        <button type="button" class="button" @click="onClickUseCurrentTime">
-          {{ $gettext('Aktuelle Position setzen') }}
-        </button>
+        <div class="start-at-setting-flex">
+          <label>
+            {{ $gettext('Anfangen um') }}
+            <input
+              type="text"
+              v-model="startAtInput"
+              @blur="onBlurStartPositionInput"
+            />
+          </label>
+          <button type="button" class="button" @click="onClickUseCurrentTime">
+            {{ $gettext('Aktuelle Position setzen') }}
+          </button>
+        </div>
+        <pre>{{ parsedStartAtInput }}</pre>
       </div>
     </fieldset>
   </form>
@@ -202,9 +274,11 @@ export default defineComponent({
   }
 }
 .start-at-setting {
-  display: flex;
-  gap: 1em;
-  align-items: flex-end;
+  > .start-at-setting-flex {
+    display: flex;
+    gap: 1em;
+    align-items: flex-end;
+  }
 }
 input[type='number'].wide {
   max-width: 12em;
