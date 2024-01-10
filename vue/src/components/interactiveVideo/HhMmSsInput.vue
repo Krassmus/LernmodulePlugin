b34ffdde3208c58d8050389e8b3d7b393cc6b777
Ann Yanich <ann.yanich@uol.de>
2024-01-10 14:55:34 +0100

    Extract component HhMmSsInput.vue. Fix small off-by-one bug

---
+++ vue/src/components/interactiveVideo/HhMmSsInput.vue
@@ -0,0 +1,98 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { $gettext } from '@/language/gettext';
+
+function formatSecondsToHhMmSs(time: number): string {
+  let hours = 0,
+    minutes = 0,
+    seconds = 0;
+  while (time >= 3600) {
+    time -= 3600;
+    hours += 1;
+  }
+  while (time >= 60) {
+    time -= 60;
+    minutes += 1;
+  }
+  seconds = time;
+  function twoDigits(n: number): string {
+    return n.toLocaleString('de-DE', {
+      minimumIntegerDigits: 2,
+      maximumFractionDigits: 0,
+    });
+  }
+  return `${twoDigits(hours)}:${twoDigits(minutes)}:${twoDigits(seconds)}`;
+}
+
+export default defineComponent({
+  name: 'HhMmSsInput',
+  props: {
+    modelValue: {
+      type: Number,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      userInput: '',
+    };
+  },
+  watch: {
+    modelValue: {
+      immediate: true,
+      handler(newValue: number) {
+        this.userInput = formatSecondsToHhMmSs(newValue);
+      },
+    },
+  },
+  methods: {
+    onBlurUserInput() {
+      if (this.parsedUserInput instanceof Error) {
+        // Reset the input to a good value
+        this.userInput = formatSecondsToHhMmSs(this.modelValue);
+      } else {
+        this.$emit('update:modelValue', this.parsedUserInput);
+      }
+    },
+  },
+  computed: {
+    /**
+     * Either the time the user input, converted into seconds, or an Error with
+     * a message explaining why the user's input was invalid.
+     */
+    parsedUserInput(): number | Error {
+      // This regex is adapted from https://stackoverflow.com/a/8318367/7359454
+      // License CC BY-SA 3.0 https://creativecommons.org/licenses/by-sa/3.0/
+      // Accessed and adapted on 10.01.2024 by Ann Yanich
+      const timeRegex = /^(?:(?:([0-9]?\d):)?([0-5]?\d):)?([0-5]?\d)$/;
+      const match = timeRegex.exec(this.userInput);
+      if (!match || match.length !== 4) {
+        return new Error(
+          $gettext('Die Startposition muss der Syntax HH:MM:SS entsprechen.')
+        );
+      }
+      // match[0] is the time string with colons,
+      // match[1] is hours (or undefined if not present),
+      // match[2] is minutes (or undefined if not present),
+      // and match[3] is seconds (always present).
+      if (match[1]) {
+        return (
+          parseInt(match[1]) * 3600 +
+          parseInt(match[2]) * 60 +
+          parseInt(match[3])
+        );
+      } else if (match[2]) {
+        return parseInt(match[2]) * 60 + parseInt(match[3]);
+      } else {
+        return parseInt(match[3]);
+      }
+    },
+  },
+});
+</script>
+
+<template>
+  <input type="text" v-model="userInput" @blur="onBlurUserInput" />
+</template>
+
+<style scoped></style>
--- vue/src/components/interactiveVideo/SelectVideo.vue
+++ vue/src/components/interactiveVideo/SelectVideo.vue
@@ -7,6 +7,7 @@ import { $gettext } from '@/language/gettext';
 import { InteractiveVideoTask } from '@/models/InteractiveVideoTask';
 import FileUpload from '@/components/FileUpload.vue';
 import { UploadedFile } from '@/routes';
+import HhMmSsInput from '@/components/interactiveVideo/HhMmSsInput.vue';
 
 function formatSecondsToHhMmSs(time: number): string {
   let hours = 0,
@@ -41,42 +42,8 @@ export default defineComponent({
   data() {
     return {
       youtubeUrlInput: '',
-      startAtInput: '',
       currentTime: 0,
     };
-  },
-  computed: {
-    /**
-     * Either the time the user input, converted into seconds, or an Error with
-     * a message explaining why the user's input was invalid.
-     */
-    parsedStartAtInput(): number | Error {
-      // This regex is adapted from https://stackoverflow.com/a/8318367/7359454
-      // License CC BY-SA 3.0 https://creativecommons.org/licenses/by-sa/3.0/
-      // Accessed and adapted on 10.01.2024 by Ann Yanich
-      const timeRegex = /^(?:(?:([0-9]?\d):)?([0-5]?\d):)?([0-5]?\d)$/;
-      const match = timeRegex.exec(this.startAtInput);
-      if (!match || match.length !== 4) {
-        return new Error(
-          $gettext('Die Startposition muss der Syntax HH:MM:SS entsprechen.')
-        );
-      }
-      // match[0] is the time string with colons,
-      // match[1] is hours (or empty string if not present),
-      // match[2] is minutes (or empty string if not present),
-      // and match[3] is seconds (always present).
-      if (match[1] !== '') {
-        return (
-          parseInt(match[1]) * 3600 +
-          parseInt(match[2]) * 60 +
-          parseInt(match[3])
-        );
-      } else if (match[2] !== '') {
-        return parseInt(match[2]) * 60 + parseInt(match[3]);
-      } else {
-        return parseInt(match[3]);
-      }
-    },
   },
   watch: {
     'taskDefinition.video': {
@@ -89,12 +56,6 @@ export default defineComponent({
         }
       },
     },
-    'taskDefinition.startAt': {
-      immediate: true,
-      handler: function (startPosition: number) {
-        this.startAtInput = formatSecondsToHhMmSs(startPosition);
-      },
-    },
   },
   methods: {
     $gettext,
@@ -103,15 +64,6 @@ export default defineComponent({
     },
     onClickUseCurrentTime() {
       this.taskDefinition.startAt = this.currentTime;
-    },
-    onBlurStartPositionInput() {
-      if (this.parsedStartAtInput instanceof Error) {
-        // Reset the input to a good value
-        this.startAtInput = formatSecondsToHhMmSs(this.taskDefinition.startAt);
-      } else {
-        // Save the value into the task definition
-        this.taskDefinition.startAt = this.parsedStartAtInput;
-      }
     },
     onSaveYoutubeVideo() {
       this.taskDefinition.video = {
@@ -131,7 +83,7 @@ export default defineComponent({
       };
     },
   },
-  components: { FileUpload, VideoPlayer },
+  components: { HhMmSsInput, FileUpload, VideoPlayer },
 });
 </script>
 
@@ -220,17 +172,13 @@ export default defineComponent({
         <div class="start-at-setting-flex">
           <label>
             {{ $gettext('Anfangen um') }}
-            <input
-              type="text"
-              v-model="startAtInput"
-              @blur="onBlurStartPositionInput"
-            />
+            <HhMmSsInput v-model="taskDefinition.startAt" />
           </label>
+          <pre>{{ taskDefinition.startAt }}</pre>
           <button type="button" class="button" @click="onClickUseCurrentTime">
             {{ $gettext('Aktuelle Position setzen') }}
           </button>
         </div>
-        <pre>{{ parsedStartAtInput }}</pre>
       </div>
     </fieldset>
   </form>
