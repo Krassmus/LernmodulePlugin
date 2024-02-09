b4b11e34d1ba0aa8eb78b177a1339c1c0bc9020d
Ann Yanich <ann.yanich@uol.de>
2024-02-09 13:48:33 +0100

    Memoize $gettext function, preventing 'No translations found' log spam.

--- vue/src/language/gettext.ts
+++ vue/src/language/gettext.ts
@@ -1,5 +1,6 @@
 import { createGettext } from 'vue3-gettext';
 import translations from '@/language/translations.json';
+import { memoize } from 'lodash';
 
 const selectedLanguage = Object.values(window.STUDIP.INSTALLED_LANGUAGES).find(
   (lang) => lang.selected
@@ -13,6 +14,11 @@ const gettextPlugin = createGettext({
   translations: translations,
 });
 
-const $gettext = gettextPlugin.$gettext;
+// Cache gettext results.
+// This fixes the problem we had where the error 'No translations found for de' was
+// being printed excessively often (sometimes 1000s of times per second) during
+// certain UI interactions, e.g. when dragging interactions around on top of
+// the video in the Interactive Video Editor.
+const $gettext = memoize(gettextPlugin.$gettext);
 
 export { gettextPlugin, $gettext };
