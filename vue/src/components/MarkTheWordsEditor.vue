8d163f31c12fd55f1c56903d6cd47697bfe3f866
Thomas Hellkamp <thomas.hellkamp@uni-oldenburg.de>
2024-03-14 21:23:20 +0100

    Mark The Words: Added possibility to define the dynamic feedback and result message in the editor

--- vue/src/components/MarkTheWordsEditor.vue
+++ vue/src/components/MarkTheWordsEditor.vue
@@ -24,6 +24,64 @@
         disable-autoformat
       />
     </fieldset>
+
+    <fieldset class="collapsable collapsed">
+      <legend>{{ $gettext('Feedback') }}</legend>
+      <label>
+        {{
+          $gettext(
+            'Ergebnismitteilung (mögliche Variablen :correct und :total):'
+          )
+        }}
+        <input
+          type="text"
+          v-model="taskDefinition.strings.resultMessage"
+          style="width: 100%"
+        />
+      </label>
+      <label>{{
+        $gettext('Benutzerdefiniertes Feedback für beliebige Punktebereiche:')
+      }}</label>
+      <div class="feedbackContainer">
+        <div class="feedbackPercentagesChild">
+          <label>
+            {{ $gettext('Prozent') }}
+          </label>
+          <template v-for="(feedback, i) in taskDefinition.feedback" :key="i">
+            <input
+              type="number"
+              min="0"
+              max="100"
+              v-model="taskDefinition.feedback[i].percentage"
+            />
+          </template>
+        </div>
+
+        <div class="feedbackMessagesChild">
+          <label>
+            {{ $gettext('Mitteilung') }}
+          </label>
+          <div
+            class="feedbackMessagesChildSubdivision"
+            v-for="(feedback, i) in taskDefinition.feedback"
+            :key="i"
+          >
+            <input type="text" v-model="taskDefinition.feedback[i].message" />
+            <button
+              type="button"
+              :title="titleForDeleteButtonForFeedback(feedback)"
+              @click="removeFeedback(feedback)"
+            >
+              <img :src="urlForIcon('trash')" width="16" height="16" />
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <button type="button" class="button" @click="addFeedback">
+        {{ $gettext('Neuer Bereich') }}
+      </button>
+    </fieldset>
   </form>
 </template>
 
@@ -32,7 +90,7 @@
 // TODO refrain from mutating taskDefinition directly -- it breaks undo/redo
 /* eslint-disable vue/no-mutating-props */
 import { defineComponent, PropType } from 'vue';
-import { MarkTheWordsTask } from '@/models/TaskDefinition';
+import { Feedback, MarkTheWordsTask } from '@/models/TaskDefinition';
 import { taskEditorStore } from '@/store';
 import { $gettext } from '@/language/gettext';
 import StudipWysiwyg from '@/components/StudipWysiwyg.vue';
@@ -48,6 +106,7 @@ export default defineComponent({
   },
   methods: {
     $gettext,
+
     /**
      * Surround the selected text with two asterisks
      */
@@ -70,6 +129,32 @@ export default defineComponent({
         writer.insertText('*', start);
       });
     },
+
+    titleForDeleteButtonForFeedback(feedback: Feedback): string {
+      return this.$gettext(
+        'Entferne den Feedback-Bereich, der ab %{ percentage }% anfängt.',
+        { percentage: feedback.percentage.toString() }
+      );
+    },
+
+    addFeedback(): void {
+      this.taskDefinition.feedback.push({
+        percentage: this.feedbackSortedByScore[0]?.percentage,
+        message: 'Feedback',
+      });
+    },
+
+    removeFeedback(feedbackToRemove: Feedback): void {
+      this.taskDefinition.feedback = this.taskDefinition.feedback.filter(
+        (feedback) => feedback !== feedbackToRemove
+      );
+    },
+
+    urlForIcon(iconName: string) {
+      return (
+        window.STUDIP.ASSETS_URL + 'images/icons/blue/' + iconName + '.svg'
+      );
+    },
   },
 
   computed: {
@@ -78,8 +163,40 @@ export default defineComponent({
         'Markieren Sie ein Wort als Lösung, indem Sie ein Sternchen (*) vor und hinter dem Wort setzen oder markieren Sie ein Wort und klicken Sie den "Richtiges Wort markieren"–Button.'
       );
     },
+
+    feedbackSortedByScore(): Feedback[] {
+      return this.taskDefinition.feedback
+        .map((value) => value)
+        .sort((a, b) => b.percentage - a.percentage);
+    },
   },
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+.feedbackContainer {
+  display: flex;
+  gap: 0.5em;
+  justify-content: flex-start;
+  align-items: center;
+  max-width: 48em;
+}
+
+.feedbackPercentagesChild {
+  flex: 0 100px;
+  display: flex;
+  flex-direction: column;
+}
+
+.feedbackMessagesChild {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+}
+
+.feedbackMessagesChildSubdivision {
+  display: flex;
+  align-items: center;
+  gap: 0.5em;
+}
+</style>
