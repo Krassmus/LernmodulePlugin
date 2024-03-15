Krassmus/LernmodulePlugin

commit 7642d0013f39edad79420f27ef899397ad7fa15b

Mark The Words: Added dynamic feedback to the task

vue/src/models/TaskDefinition.ts
@@ -70,6 +70,7 @@ export const markTheWordsTaskSchema = z.object({
     solutionsButton: z.string(),
     resultMessage: z.string(),
   }),
+  feedback: z.array(feedbackSchema),
 });
 export type MarkTheWordsTask = z.infer<typeof markTheWordsTaskSchema>;
 
@@ -354,6 +355,12 @@ export function newTask(type: TaskDefinition['task_type']): TaskDefinition {
           solutionsButton: 'Lösungen anzeigen',
           resultMessage: ':correct von :total Wörter richtig ausgewählt.',
         },
+        feedback: [
+          { percentage: 0, message: 'Versuchen Sie es noch einmal.' },
+          { percentage: 50, message: 'Gut.' },
+          { percentage: 75, message: 'Sehr gut.' },
+          { percentage: 100, message: 'Perfekt!' },
+        ],
       };
     case 'Memory':
       return {

vue/src/components/MarkTheWordsViewer.vue
@@ -19,9 +19,10 @@
 
     <feedback-element
       v-if="showResults"
-      :message="feedbackMessage"
       :achieved-points="score"
       :max-points="maxScore"
+      :feedback="this.task.feedback"
+      :result-message="resultMessage"
     />
 
     <div class="h5pButtonPanel">
@@ -227,8 +228,15 @@ export default defineComponent({
       return maxScore;
     },
 
-    feedbackMessage(): string {
-      return this.score + ' / ' + this.maxScore;
+    resultMessage(): string {
+      let resultMessage = this.task.strings.resultMessage.replace(
+        ':correct',
+        this.score.toString()
+      );
+
+      resultMessage = resultMessage.replace(':total', this.maxScore.toString());
+
+      return resultMessage;
     },
   },
 });

commit 1127d9b82940afc552b8f0dfa37ca72c2056b8fe

Add 'default()' to the newly added field of markTheWordsTaskSchema to be backwards compatible with existing tasks in the database
The change in 7642d0013f39edad79420f27ef899397ad7fa15b caused my existing tasks in my database to no longer work, because they were missing the newly added field 'feedback'. This commit fixes that issue

vue/src/models/TaskDefinition.ts
@@ -70,7 +70,7 @@ export const markTheWordsTaskSchema = z.object({
     solutionsButton: z.string(),
     resultMessage: z.string(),
   }),
-  feedback: z.array(feedbackSchema),
+  feedback: z.array(feedbackSchema).default(() => defaultFeedback()),
 });
 export type MarkTheWordsTask = z.infer<typeof markTheWordsTaskSchema>;
 
@@ -217,6 +217,15 @@ export const taskTypeSchema = z.union([
   questionTaskSchema.shape.task_type,
 ]);
 
+function defaultFeedback(): Feedback[] {
+  return [
+    { percentage: 0, message: 'Versuchen Sie es noch einmal.' },
+    { percentage: 50, message: 'Gut.' },
+    { percentage: 75, message: 'Sehr gut.' },
+    { percentage: 100, message: 'Perfekt!' },
+  ];
+}
+
 export function newTask(type: TaskDefinition['task_type']): TaskDefinition {
   switch (type) {
     case 'DragTheWords':
@@ -236,12 +245,7 @@ export function newTask(type: TaskDefinition['task_type']): TaskDefinition {
             'Alle Lücken müssen ausgefüllt sein, um Lösungen anzuzeigen.',
           resultMessage: ':correct von :total Lücken richtig ausgefüllt.',
         },
-        feedback: [
-          { percentage: 0, message: 'Versuchen Sie es noch einmal.' },
-          { percentage: 50, message: 'Gut.' },
-          { percentage: 75, message: 'Sehr gut.' },
-          { percentage: 100, message: 'Perfekt!' },
-        ],
+        feedback: defaultFeedback(),
       };
     case 'FillInTheBlanks':
       return {
@@ -261,12 +265,7 @@ export function newTask(type: TaskDefinition['task_type']): TaskDefinition {
             'Alle Lücken müssen ausgefüllt sein, um Lösungen anzuzeigen.',
           resultMessage: ':correct von :total Lücken richtig ausgefüllt.',
         },
-        feedback: [
-          { percentage: 0, message: 'Versuchen Sie es noch einmal.' },
-          { percentage: 50, message: 'Gut.' },
-          { percentage: 75, message: 'Sehr gut.' },
-          { percentage: 100, message: 'Perfekt!' },
-        ],
+        feedback: defaultFeedback(),
       };
     case 'FindTheHotspot':
       return {
@@ -301,12 +300,7 @@ export function newTask(type: TaskDefinition['task_type']): TaskDefinition {
           solutionsButton: 'Lösungen anzeigen',
           resultMessage: ':correct von :total Bilder richtig zugeordnet.',
         },
-        feedback: [
-          { percentage: 0, message: 'Versuchen Sie es noch einmal.' },
-          { percentage: 50, message: 'Gut.' },
-          { percentage: 75, message: 'Sehr gut.' },
-          { percentage: 100, message: 'Perfekt!' },
-        ],
+        feedback: defaultFeedback(),
       };
     case 'ImageSequencing':
       return {
@@ -324,12 +318,7 @@ export function newTask(type: TaskDefinition['task_type']): TaskDefinition {
           solutionsButton: 'Lösungen anzeigen',
           resultMessage: ':correct von :total Bildern richtig sortiert.',
         },
-        feedback: [
-          { percentage: 0, message: 'Versuchen Sie es noch einmal.' },
-          { percentage: 50, message: 'Gut.' },
-          { percentage: 75, message: 'Sehr gut.' },
-          { percentage: 100, message: 'Perfekt!' },
-        ],
+        feedback: defaultFeedback(),
       };
     case 'InteractiveVideo':
       return {
@@ -355,12 +344,7 @@ export function newTask(type: TaskDefinition['task_type']): TaskDefinition {
           solutionsButton: 'Lösungen anzeigen',
           resultMessage: ':correct von :total Wörter richtig ausgewählt.',
         },
-        feedback: [
-          { percentage: 0, message: 'Versuchen Sie es noch einmal.' },
-          { percentage: 50, message: 'Gut.' },
-          { percentage: 75, message: 'Sehr gut.' },
-          { percentage: 100, message: 'Perfekt!' },
-        ],
+        feedback: defaultFeedback(),
       };
     case 'Memory':
       return {