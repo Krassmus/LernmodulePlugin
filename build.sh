abafae6c8b52906d6de388c57c4800c5c712ca24
Ann Yanich <ann.yanich@uol.de>
2024-04-30 15:12:59 +0200

    Revert "Migrate to node 16 / npm 8. Add library 'verzod'"
    This reverts commit 1678f0ba40d4d74ec907d0728d9565f37e423393.

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euxo pipefail # explanation at https://gist.github.com/mohanpedala/1e2ff5661761d3abd0385e8223e16425
-if [[ ! "$(node -v)" =~ v16\..* ]] || [[ ! "$(npm -v)" =~ 8\..* ]]; then
-  echo "Please use node v16 and npm 8"
+if [[ ! "$(node -v)" =~ v14\..* ]] || [[ ! "$(npm -v)" =~ 6\..* ]]; then
+  echo "Please use node v14 and npm 6"
   exit 1;
 fi;
 timestamp=$(date "+%Y-%m-%d_%H-%M-%S") &&
