4552cda61e429ede957d197f4d9810c79def76c8
Ann Yanich <ann@yanich.de>
2024-07-12 18:45:15 +0200

    Re: #15 Add task_json to error message shown when task_json cannot be parsed

--- vue/src/courseware-main.ts
+++ vue/src/courseware-main.ts
@@ -71,11 +71,20 @@ function initializeApp(initializeMessage: InitializeMessage) {
         initializeMessage.block.attributes.payload.task_json
       );
     } catch (e: unknown) {
-      const errorMessage = $gettext(
-        'Diese Aufgabe konnte nicht geladen werden. ' +
-          'Beim Einlesen von task_json ist ein Fehler vorgekommen. Fehler: ' +
-          e
-      );
+      const errorMessage =
+        $gettext(
+          'Diese Aufgabe konnte nicht geladen werden. ' +
+            'Beim Einlesen von task_json ist ein Fehler vorgekommen. Fehler: '
+        ) +
+        '\n' +
+        e +
+        '\n' +
+        'task_json: ' +
+        JSON.stringify(
+          initializeMessage.block.attributes.payload.task_json,
+          null,
+          2
+        );
       // TODO #15 Improve the error message given by zod so it is easier for
       //  us developers to understand.
       console.error(errorMessage, e);
