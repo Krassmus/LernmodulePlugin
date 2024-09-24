be1d7737ba20f25e1ed3791bb8b8f13b785cf356
Thomas Hellkamp <thomas.hellkamp@uni-oldenburg.de>
2024-09-24 21:07:00 +0200

    Memory: Added max size to images in the memory editor

--- vue/src/components/EditedMemoryCardImage.vue
+++ vue/src/components/EditedMemoryCardImage.vue
@@ -1,8 +1,18 @@
 <template>
-  <img :src="fileIdToUrl(card.file_id)" :alt="card.altText" />
-  <button type="button" @click="deleteImage">
-    {{ $gettext('Bild Löschen') }}
-  </button>
+  <div class="memory-card-image-and-button-container">
+    <img
+      :src="fileIdToUrl(card.file_id)"
+      :alt="card.altText"
+      class="memory-card-image"
+    />
+    <button
+      type="button"
+      @click="deleteImage"
+      class="memory-card-delete-image-button"
+    >
+      {{ $gettext('Bild Löschen') }}
+    </button>
+  </div>
 </template>
 
 <script lang="ts">
@@ -42,4 +52,17 @@ export default defineComponent({
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+.memory-card-image-and-button-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 0.5em;
+}
+
+.memory-card-image {
+  max-width: 100%;
+  max-height: 480px;
+}
+</style>
