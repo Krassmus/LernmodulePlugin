257bddf633eb37c704c584f0a00e653260121b2d
Ann Yanich <ann.yanich@uol.de>
2025-12-03 23:16:05 +0100

    Fix error where interactive video viewer failed to work in "Lernmodule" tab due to missing vuex store

--- vue/src/viewer-main.ts
+++ vue/src/viewer-main.ts
@@ -3,8 +3,10 @@ import LernmoduleViewer from '@/components/LernmoduleViewer.vue';
 import { gettextPlugin } from '@/language/gettext';
 import { disableDrag } from '@/directives/vDisableDrag';
 import './assets/global.css';
+import { store } from '@/store';
 
 const app = createApp(LernmoduleViewer);
 app.directive('disable-drag', disableDrag);
+app.use(store);
 app.use(gettextPlugin);
 app.mount('#stud5p-app');
