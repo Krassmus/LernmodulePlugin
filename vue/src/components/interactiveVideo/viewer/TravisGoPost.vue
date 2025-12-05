f15b55cf5bf0c8ad56db2dc8d3ce364a598640ca
Ann Yanich <ann.yanich@uol.de>
2025-12-05 20:15:17 +0100

    Put icon indicating the type of the post in each travis go post

--- vue/src/components/interactiveVideo/viewer/TravisGoPost.vue
+++ vue/src/components/interactiveVideo/viewer/TravisGoPost.vue
@@ -1,16 +1,33 @@
 <template>
-  <div class="travis-go-post">
-    <h4>
+  <div class="travis-go-post" :data-post-type="post.type">
+    <h4 class="travis-go-post-heading">
       [{{ formatVideoTimestamp(post.start) }}
       <span class="post-type">{{ post.type }}</span> @{{ post.authorName }}]
     </h4>
-    {{ post.description }}
+    <p class="travis-go-post-description">
+      {{ post.description }}
+    </p>
   </div>
 </template>
 <style scoped lang="scss">
 .travis-go-post {
-  h4 {
-    margin-top: 5px;
+  display: grid;
+  grid-template-columns: min-content 1fr;
+  grid-template-rows: min-content 1fr;
+  grid-template-areas:
+    'icon heading'
+    '.    description';
+  grid-gap: 5px;
+  align-items: start;
+  &::before {
+    grid-area: icon;
+  }
+  .travis-go-post-heading {
+    grid-area: heading;
+    margin-top: 0;
+  }
+  .travis-go-post-description {
+    grid-area: description;
   }
   margin: 0;
   padding: 5px;
