c022f14c42af21a84de2c612a88bc741e0ccce61
Ann Yanich <ann.yanich@uol.de>
2026-01-05 17:42:26 +0100

    Revert "TravisGoPost.php use html purify on wysiwyg html 'description' field"
    This reverts commit 55927b4c9631e9cfbf7f0aa21462c42f4ad4fd43.

--- lib/models/TravisGoPost.php
+++ lib/models/TravisGoPost.php
@@ -26,7 +26,7 @@ class TravisGoPost extends SORM {
     public const EDITABLE_FIELDS = [
         'start_time' => 'float',
         'end_time' => 'float',
-        'description' => 'html-string',
+        'description' => 'string',
         'post_type' => 'int',
         'video_id' => 'string',
         'video_type' => 'string'
