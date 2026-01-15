b4922b344d5d8058e62eac4d431cb7ff5e804d6d
Ann Yanich <ann.yanich@uol.de>
2026-01-15 17:54:14 +0100

    Supply 'mayEdit' and 'mayDelete' meta attributes for travis go post and travis go comment

--- lib/schemas/TravisGoCommentSchema.php
+++ lib/schemas/TravisGoCommentSchema.php
@@ -2,9 +2,27 @@
 
 namespace LernmodulePlugin\schemas;
 
+use LernmodulePlugin\authorities\TravisGoCommentAuthority;
 use LernmodulePlugin\SchemaProvider;
 
 class TravisGoCommentSchema extends SchemaProvider
 {
     const TYPE = 'lernmodule-plugin/travis-go-comments';
+    public function hasResourceMeta($resource): bool
+    {
+        return true;
+    }
+    public function getResourceMeta($resource)
+    {
+        $parentMeta = parent::getResourceMeta($resource);
+        $authority = new TravisGoCommentAuthority();
+        $user = $this->currentUser;
+        $meta = [
+            'permissions' => [
+                'mayEdit' => $authority->mayEdit($user, $resource),
+                'mayDelete' => $authority->mayDelete($user, $resource),
+            ]
+        ];
+        return array_merge($parentMeta, $meta);
+    }
 }
--- lib/schemas/TravisGoPostSchema.php
+++ lib/schemas/TravisGoPostSchema.php
@@ -2,9 +2,27 @@
 
 namespace LernmodulePlugin\schemas;
 
+use LernmodulePlugin\authorities\TravisGoPostAuthority;
 use LernmodulePlugin\SchemaProvider;
 
 class TravisGoPostSchema extends SchemaProvider
 {
     const TYPE = 'lernmodule-plugin/travis-go-posts';
+    public function hasResourceMeta($resource): bool
+    {
+        return true;
+    }
+    public function getResourceMeta($resource)
+    {
+        $parentMeta = parent::getResourceMeta($resource);
+        $authority = new TravisGoPostAuthority();
+        $user = $this->currentUser;
+        $meta = [
+            'permissions' => [
+                'mayEdit' => $authority->mayEdit($user, $resource),
+                'mayDelete' => $authority->mayDelete($user, $resource),
+            ]
+        ];
+        return array_merge($parentMeta, $meta);
+    }
 }
