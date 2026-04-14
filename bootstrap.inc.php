f0076e4447efee59b4335f1ab6be1e0a73af6dd3
Ann Yanich <ann.yanich@uol.de>
2026-04-14 17:07:21 +0200

    Show "Fortschritt" in left-hand navigation instead of "Teilnehmende" if user is not permitted to view the "Teilnehmende" page.
    (cherry picked from commit 9cd50ae97c2e1609b5cc5b3363f68e411149b58b)

--- bootstrap.inc.php
+++ bootstrap.inc.php
@@ -2,4 +2,5 @@
 
 StudipAutoloader::addAutoloadPath(__DIR__ . '/lib');
 StudipAutoloader::addAutoloadPath(__DIR__ . '/lib', 'LernmodulePlugin');
+StudipAutoloader::addAutoloadPath(__DIR__ . '/lib/authorities', 'LernmodulePlugin');
 StudipAutoloader::addAutoloadPath(__DIR__ . '/lib/H5P');
---
+++ lib/authorities/ProgressAuthority.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace LernmodulePlugin;
+use Config;
+use User;
+
+class ProgressAuthority {
+    /**
+     * @param $context_id
+     * @param $student_id
+     * @return bool if the current user can view the progress of the given student.
+     * Users can view their own progress. Dozents (or whatever minimum status is configured
+     * in LERNMODUL_PARTICIPANT_EVALUATION) can view the progress of all students in the class
+     */
+    public static function canViewEvaluation($context_id, $student_id): bool
+    {
+        return static::canViewAllStudentsProgress($context_id) ||
+            User::findCurrent()->id == $student_id;
+    }
+
+    /**
+     * @param $context_id
+     * @return bool True iff the current user has permission to look at the progress
+     * each student has made in the Lernmodule in the given context ID.
+     */
+    public static function canViewAllStudentsProgress($context_id): bool {
+        $required_perm = Config::get()->LERNMODUL_PARTICIPANT_EVALUATION;
+        return $required_perm && $GLOBALS['perm']->have_studip_perm($required_perm, $context_id);
+    }
+
+}
