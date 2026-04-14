670d2ea76525ad85cd63e8730c26d5f73fec9caa
Ann Yanich <ann.yanich@uol.de>
2026-04-14 16:53:53 +0200

    Prevent access to "Teilnehmende" page if user doesnt have permission Config::get()->LERNMODUL_PARTICIPANT_EVALUATION or higher in the current Context

--- controllers/participants.php
+++ controllers/participants.php
@@ -6,16 +6,27 @@ class ParticipantsController extends PluginController
     public function before_filter(&$action, &$args)
     {
         parent::before_filter($action, $args);
+    }
+
+    /**
+     * @return void
+     * @throws AccessDeniedException
+     * Show a list of the participants in the current Context, including the number of
+     * Lernmodule modules they have completed, where the user can click on each student's
+     * name to view more details about their progress.
+     */
+    public function index_action()
+    {
+        $cid = Context::getId();
+        if (!isset($cid) || !$this->canViewAllStudentsProgress($cid)) {
+            throw new AccessDeniedException();
+        }
+        // TODO: Add translation for this string
+        PageLayout::setTitle(_("Teilnehmende"));
         Navigation::activateItem("/course/lernmodule/participants");
         Navigation::getItem("/course/lernmodule")->setImage(
             Icon::create("learnmodule", "info")
         );
-    }
-
-    public function index_action()
-    {
-        // TODO: Add translation for this string
-        PageLayout::setTitle(_("Teilnehmende"));
         $this->module = Lernmodul::findBySQL(
             "INNER JOIN lernmodule_courses USING (module_id) WHERE lernmodule_courses.seminar_id = ? ORDER BY name ASC",
             array(Context::get()->id)
@@ -27,43 +38,49 @@ class ParticipantsController extends PluginController
 
     /**
      * Return the 'href' attribute for a given row of the Participants table.
-     * It should link either to the student's 'Evaluation' page
-     * or to the student's Stud.IP profile, depending on the plugin's configuration
-     * and the user's status.
+     * It should link to the student's 'Evaluation' page.
      */
     public function linkForStudent($student): string
     {
-        if ($this->canViewEvaluation($student['user_id'])) {
-            return PluginEngine::getLink($this->plugin, [], "participants/evaluation/" . $student['user_id']);
-        } else {
-            return URLHelper::getLink("dispatch.php/profile", ['username' => $student['username']]);
-        }
+        return PluginEngine::getLink($this->plugin, [], "participants/evaluation/" . $student['user_id']);
     }
 
     public function evaluation_action($user_id)
     {
-        PageLayout::setTitle(_("Auswertung"));
-        if (!$this->canViewEvaluation($user_id)) {
+        $cid = Context::getId();
+        if (!isset($cid) || !$this->canViewEvaluation($cid, $user_id)) {
             throw new AccessDeniedException();
         }
+        PageLayout::setTitle(_("Auswertung"));
+        Navigation::activateItem("/course/lernmodule/participants");
+        Navigation::getItem("/course/lernmodule")->setImage(
+            Icon::create("learnmodule", "info")
+        );
         $this->user_id = $user_id;
-        $this->attempts = LernmodulAttempt::findByUserAndCourse($this->user_id, Context::get()->id);
+        $this->attempts = LernmodulAttempt::findByUserAndCourse($this->user_id, $cid);
     }
 
     /**
+     * @param $context_id
      * @param $student_id
      * @return bool if the current user can view the evaluation for the given student.
      * Users can view their own evaluation. Dozents (or whatever minimum status is configured
      * in LERNMODUL_PARTICIPANT_EVALUATION) can view the evaluation of all students in the class
      */
-    public function canViewEvaluation($student_id): bool
+    public function canViewEvaluation($context_id, $student_id): bool
     {
-        return (Context::getId() &&
-                Config::get()->LERNMODUL_PARTICIPANT_EVALUATION &&
-                $GLOBALS['perm']->have_studip_perm(
-                    Config::get()->LERNMODUL_PARTICIPANT_EVALUATION,
-                    Context::getId()))
-            || (User::findCurrent()->id == $student_id);
+        return $this->canViewAllStudentsProgress($context_id) ||
+            User::findCurrent()->id == $student_id;
+    }
+
+    /**
+     * @param $context_id
+     * @return bool True iff the current user has permission to look at the progress
+     * each student has made in the Lernmodule in the given context ID.
+     */
+    public function canViewAllStudentsProgress($context_id): bool {
+        $required_perm = Config::get()->LERNMODUL_PARTICIPANT_EVALUATION;
+        return $required_perm && $GLOBALS['perm']->have_studip_perm($required_perm, $context_id);
     }
 
 }
