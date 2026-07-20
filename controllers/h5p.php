3b0662c2fcfa5a92b1e84c72353bbb2b9fadd761
Ann Yanich <ann.yanich@uol.de>
2026-07-20 12:13:03 +0200

    Hotfix - Remove accidental double-negative in permissions check for updating LernmodulAttempt

--- controllers/h5p.php
+++ controllers/h5p.php
@@ -228,7 +228,7 @@ class H5pController extends PluginController
             throw new AccessDeniedException();
         }
         $this->attempt = new LernmodulAttempt(Request::get("a"));
-        if (!$this->attempt['user_id'] !== User::findCurrent()->id) {
+        if ($this->attempt['user_id'] !== User::findCurrent()->id) {
             throw new AccessDeniedException();
         }
         if (!$this->mod->isAllowed()) {
