4317c3080acc6834f3430f85c4a5c233205f0934
Ann Yanich <ann.yanich@uol.de>
2026-07-31 17:19:57 +0200

    controllers/html.php Correct typo in get_url_action (forgotten ! in guard)

--- controllers/html.php
+++ controllers/html.php
@@ -44,7 +44,7 @@ class HtmlController extends PluginController
     public function get_url_action($module_id)
     {
         $this->module = Lernmodul::find($module_id);
-        if ($this->module || !$this->module->isWritable()) {
+        if (!$this->module || !$this->module->isWritable()) {
             throw new AccessDeniedException();
         }
         PageLayout::setTitle(dgettext("lernmoduleplugin","Direktlink zum Lernmodul"));
