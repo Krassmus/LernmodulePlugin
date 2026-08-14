c77eb8aa5928d1d740452b80ba5c9a984be4f89b
Ann Yanich <ann.yanich@uol.de>
2026-08-14 14:56:52 +0200

    Don't include "a" parameter in the link in the "URL-Kopieren" box for H5P Lernmodule
    This is the 'attempt id' parameter and it makes the link invalid for anyone who
    is not the user who created the link

--- views/h5p/get_url.php
+++ views/h5p/get_url.php
@@ -3,7 +3,7 @@
     <label>
         <?= dgettext("lernmoduleplugin","URL") ?>
         <? $oldbase = URLHelper::setBaseURL($GLOBALS['ABSOLUTE_URI_STUDIP']) ?>
-        <input type="text" readonly value="<?= URLHelper::getLink("plugins.php/lernmoduleplugin/h5p/iframe/".$module->getId(), array('a' => $attempt->getId())) ?>">
+        <input type="text" readonly value="<?= URLHelper::getLink("plugins.php/lernmoduleplugin/h5p/iframe/".$module->getId(), array()) ?>">
         <? URLHelper::setBaseURL($oldbase) ?>
     </label>
 
